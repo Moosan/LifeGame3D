b0a6b3b162c8a53328d80a63e185f39a681fbcc3
Moosan <washer2016@outlook.jp>
2017-04-27 12:59:17 +0900

    ちょいちょい

--- LifeGame3D/Assets/LifePrefab.cs
+++ LifeGame3D/Assets/LifePrefab.cs
@@ -8,16 +8,31 @@ public class LifePrefab : MonoBehaviour
     private Vector3 pos;
     private Vector3[] positions;
     public Axis DefaltMoveDirection;
-    public Axis Axis { get; set; }
+    public Axis Axis;
+    public bool moveFoward;
     public void Start()
     {
         Vector3 position = transform.position;
         pos = new Vector3((int)position.x,(int)position.y,(int)position.z);
-        //向きを変えれるようにしたいけどまあそこはおいおい
+        //向きを変えれるようにしたつもりなんだけどなんも変わんない辛い
         var len = PresetPos.Length;
+        if (DefaltMoveDirection != Axis) {
+            Vector3 rotateAxis = DefaltMoveDirection == Axis.x ? (DefaltMoveDirection == Axis.y ? (Axis == Axis.x ? Vector3.forward : Vector3.left) : (Axis == Axis.x ? Vector3.down : Vector3.right)) : (Axis == Axis.y ? Vector3.back : Vector3.up);
+            for(int i = 0; i < len; i++)
+            {
+                PresetPos[i] = Quaternion.Euler(90 * rotateAxis) * (PresetPos[i]-CenterPos);
+            }
+        }
+        if (!moveFoward)
+        {
+            for(int i = 0; i < len; i++)
+            {
+                PresetPos[i] = -PresetPos[i];
+            }
+        }
         positions = new Vector3[len];
         for (int i = 0; i < len; i++) {
-            positions[i] = pos + PresetPos[i]-CenterPos;
+            positions[i] = pos + PresetPos[i];
         }
     }
     public void Update()
